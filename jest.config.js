44a0525543cdfca0f32eb77d587f8da7e1b9dae9
learngermanghana <learngermanghana@gmail.com>
2025-12-19 19:21:05 +0000

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  roots: ["<rootDir>/functions/functionz/__tests__", "<rootDir>/functions/functionz/tests"],
-  testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
-  clearMocks: true,
-};
