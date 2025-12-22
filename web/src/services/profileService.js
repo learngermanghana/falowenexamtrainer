e7f7c591b03e7cfbb651c28b149de188439f87c9
learngermanghana <learngermanghana@gmail.com>
2025-12-22 15:18:50 +0000

    Delete web directory

--- web/src/services/profileService.js
+++
@@ -1,5 +0,0 @@
-import { callAI } from "./aiClient";
-
-export async function correctBiography({ text, level, idToken, timeoutMs = 15000 }) {
-  return callAI({ path: "/profile/biography/correct", payload: { text, level }, idToken, timeoutMs });
-}
