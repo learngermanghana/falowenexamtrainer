8f9209e73ef73b13b1b39810e6bb1a56e8c1321e
learngermanghana <learngermanghana@gmail.com>
2026-02-04 15:49:31 +0100

    Delete web/src directory

--- web/src/services/discussionService.js
+++
@@ -1,5 +0,0 @@
-import { callAI } from "./aiClient";
-
-export async function correctDiscussionText({ text, level, idToken, timeoutMs = 15000 }) {
-  return callAI({ path: "/discussion/correct", payload: { text, level }, idToken, timeoutMs });
-}
