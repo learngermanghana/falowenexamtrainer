93449d4228cc2586a37ed2c37315334815a7e352
learngermanghana <learngermanghana@gmail.com>
2026-02-25 13:36:48 +0000

    Update A1 chapter 1.1 grammarbook to in-app course page

--- web/src/data/courseSchedule.js
+++ web/src/data/courseSchedule.js
@@ -480,7 +480,7 @@ const RAW_COURSE_SCHEDULES = {
           chapter: "1.1",
           video: "https://youtu.be/AjsnO1hxDs4",
           youtube_link: "https://youtu.be/AjsnO1hxDs4",
-          grammarbook_link: "https://drive.google.com/file/d/1DKhyi-43HX1TNs8fxA9bgRvhylubilBf/view?usp=sharing",
+          grammarbook_link: "https://www.falowen.app/campus/course/singular-pronouns-verb-conjugation-day-2",
           assignment: true,
           workbook_link: "https://drive.google.com/file/d/1A1D1pAssnoncF1JY0v54XT2npPb6mQZv/view?usp=sharing",
         },
