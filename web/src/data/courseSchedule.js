dea6e54ce89ba925463a9ca6c0640318bc2e69e6
learngermanghana <learngermanghana@gmail.com>
2026-02-28 12:38:24 +0100

    Use Drive workbook link for Day 1 small talk

--- web/src/data/courseSchedule.js
+++ web/src/data/courseSchedule.js
@@ -48,7 +48,7 @@ const A2_SCHEDULE = [
     video: "https://youtu.be/siF0jWZdIwk",
     youtube_link: "https://youtu.be/siF0jWZdIwk",
     grammarbook_link: "/campus/course/a2-starter-conjunctions-day-1",
-    workbook_link: "/campus/course/a2-small-talk-1-1-workbook",
+    workbook_link: "https://drive.google.com/file/d/1LXDI1yyJ4aT4LhX5eGDbKnkCkJZ2EE2T/view?usp=sharing",
   },
   {
     day: 2,
