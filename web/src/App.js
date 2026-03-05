0c1d70c98def386cb965aa0dff034d4459017fab
learngermanghana <learngermanghana@gmail.com>
2026-03-05 09:33:31 +0100

    Consolidate A2 day 1 small talk workbook to enhanced page

--- web/src/App.js
+++ web/src/App.js
@@ -34,7 +34,6 @@ import LetterWritingIntroPage from "./components/LetterWritingIntroPage";
 import WeatherPerfektLetterPage from "./components/WeatherPerfektLetterPage";
 import HealthBodyPartsPage from "./components/HealthBodyPartsPage";
 import A2StarterConjunctionsPage from "./components/A2StarterConjunctionsPage";
-import A2Day2SmallTalkWorkbookPage from "./components/A2Day2SmallTalkWorkbookPage";
 import A2Day2SmallTalkWorkbookEnhancedPage from "./components/A2Day2SmallTalkWorkbookEnhancedPage";
 import A2Day2PersonenBeschreibenWorkbookPage from "./components/A2Day2PersonenBeschreibenWorkbookPage";
 import WritingPage from "./components/WritingPage";
@@ -535,8 +534,7 @@ const AppShell = ({
           />
           <Route path="/campus/course/conjunctions-5-10" element={<ConjunctionNotesPage />} />
           <Route path="/campus/course/a2-starter-conjunctions-day-1" element={<A2StarterConjunctionsPage />} />
-          <Route path="/campus/course/a2-day-2-small-talk-workbook" element={<A2Day2SmallTalkWorkbookPage />} />
-          <Route path="/campus/course/a2-day-2-small-talk-workbook-enhanced" element={<A2Day2SmallTalkWorkbookEnhancedPage />} />
+          <Route path="/campus/course/a2-day-2-small-talk-workbook" element={<A2Day2SmallTalkWorkbookEnhancedPage />} />
           <Route path="/campus/course/a2-day-2-personen-beschreiben-workbook" element={<A2Day2PersonenBeschreibenWorkbookPage />} />
           <Route path="/campus/course/weather-perfekt-letter-13" element={<WeatherPerfektLetterPage />} />
           <Route path="/campus/course/health-and-body-parts-14-1" element={<HealthBodyPartsPage />} />
