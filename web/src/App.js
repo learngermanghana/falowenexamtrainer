fb2f497692aa7bfacf0dbf7cafd16e3594c27a94
learngermanghana <learngermanghana@gmail.com>
2026-03-05 08:40:35 +0100

    Add speaking self-practice confidence link to Teil 1

--- web/src/App.js
+++ web/src/App.js
@@ -34,6 +34,7 @@ import LetterWritingIntroPage from "./components/LetterWritingIntroPage";
 import WeatherPerfektLetterPage from "./components/WeatherPerfektLetterPage";
 import HealthBodyPartsPage from "./components/HealthBodyPartsPage";
 import A2StarterConjunctionsPage from "./components/A2StarterConjunctionsPage";
+import A2Day2SmallTalkWorkbookPage from "./components/A2Day2SmallTalkWorkbookPage";
 import WritingPage from "./components/WritingPage";
 import VocabExamPage from "./components/VocabExamPage";
 import { useAuth } from "./context/AuthContext";
@@ -532,6 +533,7 @@ const AppShell = ({
           />
           <Route path="/campus/course/conjunctions-5-10" element={<ConjunctionNotesPage />} />
           <Route path="/campus/course/a2-starter-conjunctions-day-1" element={<A2StarterConjunctionsPage />} />
+          <Route path="/campus/course/a2-day-2-small-talk-workbook" element={<A2Day2SmallTalkWorkbookPage />} />
           <Route path="/campus/course/weather-perfekt-letter-13" element={<WeatherPerfektLetterPage />} />
           <Route path="/campus/course/health-and-body-parts-14-1" element={<HealthBodyPartsPage />} />
           <Route path="/campus/course/forming-basic-statements-german-a1-day-8" element={<FormingBasicStatementsPage />} />
