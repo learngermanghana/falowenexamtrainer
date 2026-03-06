9c6f37ea463bce181495bf107db15627e658decd
learngermanghana <learngermanghana@gmail.com>
2026-03-06 17:48:40 +0100

    Update Day 24 conjunctions video link

--- web/src/data/courseSchedule.js
+++ web/src/data/courseSchedule.js
@@ -875,8 +875,8 @@ const RAW_COURSE_SCHEDULES = {
       grammar_topic: "German Conjunctions",
       assignment: false,
       schreiben_sprechen: {
-        video: "https://youtu.be/isTL6k7gMMA",
-        youtube_link: "https://youtu.be/isTL6k7gMMA",
+        video: "https://youtu.be/8l1LiXGYqFA",
+        youtube_link: "https://youtu.be/8l1LiXGYqFA",
         workbook_link: "/campus/course/conjunctions-5-10",
         assignment: false,
       },
