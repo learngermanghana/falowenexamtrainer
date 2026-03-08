dcadb9b30aa573b94697b37d6736f19ac4f9c361
learngermanghana <learngermanghana@gmail.com>
2026-03-08 17:37:53 +0000

    Fix unused presentation upgrade import

--- web/src/components/SpeechTrainerPage.js
+++ web/src/components/SpeechTrainerPage.js
@@ -5,7 +5,6 @@ import { useAuth } from "../context/AuthContext";
 import {
   loadPresentationSessions,
   requestPresentationCoachReply,
-  requestPresentationUpgrade,
   savePresentationSession,
   updatePresentationSession,
 } from "../services/presentationCoachService";
