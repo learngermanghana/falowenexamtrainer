3defbd780b052c45cb45216dd5db4ca9ff02955e
learngermanghana <learngermanghana@gmail.com>
2026-03-12 13:44:16 +0000

    Add A1 Day 16 Kapitel 10 in-app workbook page

--- web/src/App.js
+++ web/src/App.js
@@ -62,6 +62,7 @@ import B1Day28KlimafreundlichLebenWorkbookPage from "./components/B1Day28Klimafr
 import A1Day14ModalVerbsWorkbookPage from "./components/A1Day14ModalVerbsWorkbookPage";
 import A1Day16FoodAndNegationGrammarPage from "./components/A1Day16FoodAndNegationGrammarPage";
 import A1Day16FoodAndDailyLifeWorkbookPage from "./components/A1Day16FoodAndDailyLifeWorkbookPage";
+import A1Day16FoodAndNegationKapitel10WorkbookPage from "./components/A1Day16FoodAndNegationKapitel10WorkbookPage";
 import A1Day4NumbersForBeginnersWorkbookPage from "./components/A1Day4NumbersForBeginnersWorkbookPage";
 import A1Chapter3AskingAboutPricesWorkbookPage from "./components/A1Chapter3AskingAboutPricesWorkbookPage";
 import A1Chapter5GermanCasesWorkbookPage from "./components/A1Chapter5GermanCasesWorkbookPage";
@@ -631,6 +632,10 @@ const AppShell = ({
           <Route
             path="/campus/course/a1-day-16-food-and-negation-food-and-daily-life-workbook"
             element={<A1Day16FoodAndDailyLifeWorkbookPage />}
+          />
+          <Route
+            path="/campus/course/a1-day-16-food-and-negation-kapitel-10-workbook"
+            element={<A1Day16FoodAndNegationKapitel10WorkbookPage />}
           />
           <Route
             path="/campus/course/a1-day-4-numbers-for-beginners-workbook"
