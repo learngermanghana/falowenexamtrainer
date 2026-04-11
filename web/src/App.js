be0c21bc610c6c02018556f75441df06928ffd75
learngermanghana <learngermanghana@gmail.com>
2026-04-11 18:21:40 +0000

    Add A2 Day 18 Die Bank anrufen in-app workbook

--- web/src/App.js
+++ web/src/App.js
@@ -80,6 +80,7 @@ import A2Day14BerufUndKarriereWorkbookPage from "./components/A2Day14BerufUndKar
 import A2Day15MeinLieblingssportWorkbookPage from "./components/A2Day15MeinLieblingssportWorkbookPage";
 import A2Day16WohlbefindenUndEntspannungWorkbookPage from "./components/A2Day16WohlbefindenUndEntspannungWorkbookPage";
 import A2Day17InDieApothekeGehenWorkbookPage from "./components/A2Day17InDieApothekeGehenWorkbookPage";
+import A2Day18DieBankAnrufenWorkbookPage from "./components/A2Day18DieBankAnrufenWorkbookPage";
 import B1Day20WieWirdManWorkbookPage from "./components/B1Day20WieWirdManWorkbookPage";
 import B1Day21LebensformenHeuteWorkbookPage from "./components/B1Day21LebensformenHeuteWorkbookPage";
 import B1Day22BeziehungWichtigWorkbookPage from "./components/B1Day22BeziehungWichtigWorkbookPage";
@@ -691,6 +692,7 @@ const AppShell = ({
             path="/campus/course/a2-day-17-in-die-apotheke-gehen-workbook"
             element={<A2Day17InDieApothekeGehenWorkbookPage />}
           />
+          <Route path="/campus/course/a2-day-18-die-bank-anrufen-workbook" element={<A2Day18DieBankAnrufenWorkbookPage />} />
           <Route path="/campus/course/b1-day-20-wie-wird-man-ausbildung-und-qualifikationen-workbook" element={<B1Day20WieWirdManWorkbookPage />} />
           <Route path="/campus/course/b1-day-21-lebensformen-heute-workbook" element={<B1Day21LebensformenHeuteWorkbookPage />} />
           <Route path="/campus/course/b1-day-22-was-ist-dir-in-einer-beziehung-wichtig-workbook" element={<B1Day22BeziehungWichtigWorkbookPage />} />
