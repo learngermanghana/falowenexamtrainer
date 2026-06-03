aece951232073b428d11f715f2743e54e29c6277
learngermanghana <learngermanghana@gmail.com>
2026-06-03 14:41:14 +0000

    Simplify self-learning registry with B2 lesson index

--- web/src/components/SelfLearningLessonRegistry.js
+++ web/src/components/SelfLearningLessonRegistry.js
@@ -2,17 +2,7 @@ import React from "react";
 import SelfLearningEditableLessonPageV2 from "./SelfLearningEditableLessonPageV2";
 import { buildDefaultLesson } from "../data/selfLearningLessons/buildSelfLearningLesson";
 import c1Day1ZieleUndLernweg from "../data/selfLearningLessons/c1/day1ZieleUndLernweg";
-import b2Day1PersoenlicheIdentitaet from "../data/selfLearningLessons/b2/day1PersoenlicheIdentitaet";
-import b2Day2AlltagUndZeitmanagement from "../data/selfLearningLessons/b2/day2AlltagUndZeitmanagement";
-import b2Day3ArbeitUndBeruf from "../data/selfLearningLessons/b2/day3ArbeitUndBeruf";
-import b2Day4BildungUndLernen from "../data/selfLearningLessons/b2/day4BildungUndLernen";
-import b2Day5GesundheitUndWohlbefinden from "../data/selfLearningLessons/b2/day5GesundheitUndWohlbefinden";
-import b2Day6MedienUndDigitaleKommunikation from "../data/selfLearningLessons/b2/day6MedienUndDigitaleKommunikation";
-import b2Day7UmweltUndNachhaltigkeit from "../data/selfLearningLessons/b2/day7UmweltUndNachhaltigkeit";
-import b2Day8ReisenUndMobilitaet from "../data/selfLearningLessons/b2/day8ReisenUndMobilitaet";
-import b2Day9WohnenUndNachbarschaft from "../data/selfLearningLessons/b2/day9WohnenUndNachbarschaft";
-import b2Day10KonsumUndGeld from "../data/selfLearningLessons/b2/day10KonsumUndGeld";
-import b2Day11GesellschaftUndIntegration from "../data/selfLearningLessons/b2/day11GesellschaftUndIntegration";
+import b2SelfLearningLessons from "../data/selfLearningLessons/b2";
 
 const c1FallbackLessons = [
   [2, "1.2", "Kultur und Identität", "Kulturelle Prägung, Zugehörigkeit und Selbstverständnis"],
@@ -44,41 +34,8 @@ const c1FallbackLessons = [
   [28, "6.3", "Review und Transfer", "C1-Themen wiederholen und auf neue Aufgaben übertragen"],
 ].map(([day, chapter, title, topic]) => buildDefaultLesson({ level: "C1", day, chapter, title, topic }));
 
-const b2FallbackLessons = [
-  [12, "3.2", "Kultur und Freizeit", "Hobbys, kulturelle Angebote und persönliche Interessen"],
-  [13, "3.3", "Familie und Generationen", "Familienmodelle, Generationenkonflikte und Verantwortung beschreiben"],
-  [14, "3.4", "Freundschaft und soziale Beziehungen", "Beziehungen, Vertrauen und soziale Erwartungen erklären"],
-  [15, "3.5", "Ernährung und Konsumverhalten", "Essgewohnheiten, gesunde Ernährung und Konsumentscheidungen diskutieren"],
-  [16, "4.1", "Digitalisierung im Alltag", "Digitale Werkzeuge, Chancen und Abhängigkeiten bewerten"],
-  [17, "4.2", "Mobilität und Stadtleben", "Verkehr, Stadtplanung und Lebensqualität vergleichen"],
-  [18, "4.3", "Natur, Klima und Verantwortung", "Klimaschutz, Natur und persönliche Verantwortung erklären"],
-  [19, "4.4", "Freiwilligenarbeit und Engagement", "Ehrenamt, gesellschaftliche Hilfe und persönliche Motivation diskutieren"],
-  [20, "4.5", "Technologie und Arbeit der Zukunft", "Automatisierung, neue Berufe und berufliche Kompetenzen beschreiben"],
-  [21, "5.1", "Migration und neue Lebenswege", "Umzug, Integration und persönliche Chancen erklären"],
-  [22, "5.2", "Demokratie und Mitbestimmung", "Beteiligung, Rechte und gesellschaftliche Verantwortung diskutieren"],
-  [23, "5.3", "Work-Life-Balance", "Arbeit, Freizeit, Erholung und Grenzen im Alltag erklären"],
-  [24, "5.4", "Wissenschaft und Forschung im Alltag", "Forschung, Medizin, Technik und Nutzen für die Gesellschaft beschreiben"],
-  [25, "5.5", "Nachhaltiger Konsum", "Kaufverhalten, Ressourcen und Verantwortung kritisch bewerten"],
-  [26, "6.1", "Behörden, Termine und formelle Kommunikation", "Formelle Anliegen, Termine und schriftliche Kommunikation trainieren"],
-  [27, "6.2", "Prüfungstraining: Argumentieren und Reagieren", "Mündliche und schriftliche B2-Prüfungsstrategien anwenden"],
-  [28, "6.3", "Review und persönlicher Fortschritt", "B2-Themen wiederholen, Schwächen erkennen und nächsten Lernplan erstellen"],
-].map(([day, chapter, title, topic]) => buildDefaultLesson({ level: "B2", day, chapter, title, topic }));
-
 export const SELF_LEARNING_LESSONS = {
-  B2: [
-    b2Day1PersoenlicheIdentitaet,
-    b2Day2AlltagUndZeitmanagement,
-    b2Day3ArbeitUndBeruf,
-    b2Day4BildungUndLernen,
-    b2Day5GesundheitUndWohlbefinden,
-    b2Day6MedienUndDigitaleKommunikation,
-    b2Day7UmweltUndNachhaltigkeit,
-    b2Day8ReisenUndMobilitaet,
-    b2Day9WohnenUndNachbarschaft,
-    b2Day10KonsumUndGeld,
-    b2Day11GesellschaftUndIntegration,
-    ...b2FallbackLessons,
-  ],
+  B2: b2SelfLearningLessons,
   C1: [c1Day1ZieleUndLernweg, ...c1FallbackLessons],
 };
 
