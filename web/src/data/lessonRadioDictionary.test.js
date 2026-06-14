bf108a6a2a5eb1fba60b36b27319cf5718470df3
learngermanghana <learngermanghana@gmail.com>
2026-06-14 09:03:25 +0000

    Cover A2 Day 26 Falowen Radio

--- web/src/data/lessonRadioDictionary.test.js
+++ web/src/data/lessonRadioDictionary.test.js
@@ -4,6 +4,7 @@ test.each([
   [9, "BD663tMiWpg"],
   [15, "WbsTFqIT058"],
   [16, "6lq6uWK1wAs"],
+  [26, "9OVfA1B-nuU"],
 ])("A2 Day %i radio remains available", (day, id) =>
   expect(getLessonRadioResource("A2", day).youtubeId).toBe(id),
 );
@@ -17,6 +18,8 @@ test.each([
   ["/campus/course/a2-day-15-mein-lieblingssport-workbook", { level: "A2", day: 15 }],
   ["/campus/course/lesson/A2/16", { level: "A2", day: 16 }],
   ["/campus/course/a2-day-16-wohlbefinden-und-entspannung-workbook", { level: "A2", day: 16 }],
+  ["/campus/course/lesson/A2/26", { level: "A2", day: 26 }],
+  ["/campus/course/a2-day-26-gefuehle-in-verschiedenen-situationen-workbook", { level: "A2", day: 26 }],
 ])("detects radio route %s", (path, expected) =>
   expect(resolveLessonRoute(path)).toEqual(expected),
 );
