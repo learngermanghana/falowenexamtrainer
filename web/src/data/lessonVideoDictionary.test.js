b2592e7284b90bd1ff25f859d4d4659e4aea0e32
learngermanghana <learngermanghana@gmail.com>
2026-06-14 09:31:15 +0000

    Cover A2 Day 14 AI video

--- web/src/data/lessonVideoDictionary.test.js
+++ web/src/data/lessonVideoDictionary.test.js
@@ -62,6 +62,7 @@ describe("getLessonVideoResources", () => {
     [8, "https://youtu.be/jXc0Krx6EpQ"],
     [11, "https://youtu.be/qPZ44s10O04?si=S0eysGDwS30WrGZ-"],
     [13, "https://youtu.be/derL046nbF8?si=VP2St42knZFk3NKo"],
+    [14, "https://youtu.be/qWy7yMgwmvQ"],
     [16, "https://youtu.be/Yt_vBwfoDBk?si=mg5pzqUvaGMZZtyR"],
     [18, "https://youtu.be/jdhvRlRMiIc"],
     [19, "https://youtu.be/aL_CJ75l11s?si=mQTO5LEU3SsOj5xe"],
