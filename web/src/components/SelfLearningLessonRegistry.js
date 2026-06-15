f25620ee7919e840651d6eb531e3aa5620f77cec
learngermanghana <learngermanghana@gmail.com>
2026-06-15 22:03:28 +0000

    feat: register full C1 day 13 lesson

--- web/src/components/SelfLearningLessonRegistry.js
+++ web/src/components/SelfLearningLessonRegistry.js
@@ -17,6 +17,7 @@ import c1Day9KonsumUndWerbung from "../data/selfLearningLessons/c1/day9KonsumUnd
 import c1Day10IntegrationUndGesellschaft from "../data/selfLearningLessons/c1/day10IntegrationUndGesellschaft";
 import c1Day11EngagementUndEhrenamt from "../data/selfLearningLessons/c1/day11EngagementUndEhrenamt";
 import c1Day12FreizeitUndKultur from "../data/selfLearningLessons/c1/day12FreizeitUndKultur";
+import c1Day13Mehrsprachigkeit from "../data/selfLearningLessons/c1/day13Mehrsprachigkeit";
 import b2Day0Orientation from "../data/selfLearningLessons/b2/day0Orientation";
 import b2Day1PersoenlicheIdentitaet from "../data/selfLearningLessons/b2/day1PersoenlicheIdentitaet";
 import b2Day2AlltagUndZeitmanagement from "../data/selfLearningLessons/b2/day2AlltagUndZeitmanagement";
@@ -25,7 +26,6 @@ import b2Day4BildungUndLernen from "../data/selfLearningLessons/b2/day4BildungUn
 import b2Day5GesundheitUndWohlbefinden from "../data/selfLearningLessons/b2/day5GesundheitUndWohlbefinden";
 
 const c1FallbackLessons = [
-  [13, "3.3", "Mehrsprachigkeit", "Sprachenlernen, Identität und Kommunikation"],
   [14, "3.4", "Innovation und Zukunft", "Technologische Entwicklung und gesellschaftlicher Wandel"],
   [15, "3.5", "Bildung und lebenslanges Lernen", "Lernen, Weiterbildung und Chancengleichheit"],
   [16, "4.1", "Technologie im Alltag", "Digitale Werkzeuge, Abhängigkeit und praktische Nutzung"],
@@ -93,6 +93,7 @@ export const SELF_LEARNING_LESSONS = {
     c1Day10IntegrationUndGesellschaft,
     c1Day11EngagementUndEhrenamt,
     c1Day12FreizeitUndKultur,
+    c1Day13Mehrsprachigkeit,
     ...c1FallbackLessons,
   ],
 };
