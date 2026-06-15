c46d509a28600eff02d9aa59e39a631ed260cc55
learngermanghana <learngermanghana@gmail.com>
2026-06-15 19:51:24 +0000

    feat: mount compact C1 lesson page for day 7

--- web/src/components/StandardLessonWritingCoachPage.js
+++ web/src/components/StandardLessonWritingCoachPage.js
@@ -1,8 +1,11 @@
 import React from "react";
 import StandardFourStageLessonPage from "./StandardFourStageLessonPageV3";
+import CompactC1LessonPage from "./CompactC1LessonPage";
 
 export const shouldMountMarkMyLetter = () => false;
 
 export default function StandardLessonWritingCoachPage({ lesson, canonicalLesson }) {
-  return <StandardFourStageLessonPage lesson={lesson} canonicalLesson={canonicalLesson} />;
+  const isCompactC1Day7 = String(lesson?.level || "").toUpperCase() === "C1" && Number(lesson?.day) === 7;
+  const LessonPage = isCompactC1Day7 ? CompactC1LessonPage : StandardFourStageLessonPage;
+  return <LessonPage lesson={lesson} canonicalLesson={canonicalLesson} />;
 }
