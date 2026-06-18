f8c15a2e7796a7740964fbc924195a4f6984003e
learngermanghana <learngermanghana@gmail.com>
2026-06-18 16:21:05 +0000

    Use requested radio for C1 Medien lesson

--- web/src/components/StandardLessonWritingCoachPage.js
+++ web/src/components/StandardLessonWritingCoachPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import StandardFourStageLessonPage from "./StandardFourStageLessonPageV3";
 import CompactC1LessonPage from "./CompactC1LessonPage";
+import { C1_DAY3_RADIO_OVERRIDE } from "../data/c1Day3RadioOverride";
 
 export const shouldMountMarkMyLetter = () => false;
 
@@ -9,5 +10,15 @@ export default function StandardLessonWritingCoachPage({ lesson, canonicalLesson
   const day = Number(lesson?.day || 0);
   const isCompactC1Lesson = level === "C1" && (day === 3 || (day >= 7 && day <= 16));
   const LessonPage = isCompactC1Lesson ? CompactC1LessonPage : StandardFourStageLessonPage;
-  return <LessonPage lesson={lesson} canonicalLesson={canonicalLesson} />;
+  const resolvedCanonicalLesson = level === "C1" && day === 3
+    ? {
+        ...(canonicalLesson || {}),
+        resources: {
+          ...(canonicalLesson?.resources || {}),
+          falowenRadio: C1_DAY3_RADIO_OVERRIDE,
+        },
+      }
+    : canonicalLesson;
+
+  return <LessonPage lesson={lesson} canonicalLesson={resolvedCanonicalLesson} />;
 }
