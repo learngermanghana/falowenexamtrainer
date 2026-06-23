ccd58ebe3325d753f22cb6440e264bee88229dda
learngermanghana <learngermanghana@gmail.com>
2026-06-23 16:18:34 +0000

    Add workbook Hören video configuration

---
+++ web/src/components/WorkbookHoerenVideoConfig.js
@@ -0,0 +1,4 @@
+export const WORKBOOK_HOEREN_VIDEOS = {
+  b1Day3: { youtubeId: "bINimMVUjCc", youtubeUrl: "https://youtu.be/bINimMVUjCc" },
+  a2Day15: { youtubeId: "p_OE59m0J-Y", youtubeUrl: "https://youtu.be/p_OE59m0J-Y" },
+};
