2adf99262ee4333f2545ba2bf3bcd27f602461c5
learngermanghana <learngermanghana@gmail.com>
2026-06-23 18:05:28 +0000

    Show A1 Day 18 workbook controls only in workbook view

--- web/src/components/AutoWorkbookStartGuide.js
+++ web/src/components/AutoWorkbookStartGuide.js
@@ -6,20 +6,28 @@ import CourseWorkbookSubmissionTabs from "./CourseWorkbookSubmissionTabs";
 import WorkbookContextSync from "./WorkbookContextSync";
 import WorkbookStartGuide from "./WorkbookStartGuide";
 
+const A1_DAY18_CHAPTER122_PATH = "/campus/course/a1-12-2-dative-articles-mit-bei-zu";
 const workbookRouteIndex = buildWorkbookRouteIndex();
 export const SELF_MANAGED_WORKBOOK_SUBMISSION_PATHS = new Set([
   "/campus/course/a1-day-2-german-alphabet-reviewing-workbook",
   "/campus/course/a1-day-2-kapitel-1-1-workbook",
   "/campus/course/a1-day-3-german-alphabet-reviewing-workbook",
   "/campus/course/a1-day-16-food-and-negation-kapitel-10-workbook",
+  A1_DAY18_CHAPTER122_PATH,
 ]);
 
 export const shouldRenderWorkbookGuide = ({ pathname = "", search = "", match } = {}) => {
   if (!match) return false;
   const normalizedPathname = normalizeInAppPath(pathname);
+  const requestedView = new URLSearchParams(search || "").get("view");
+
+  if (normalizedPathname === A1_DAY18_CHAPTER122_PATH) {
+    return requestedView === "workbook";
+  }
+
   const isB1LessonRoute = /^\/campus\/course\/lesson\/B1\/\d+$/i.test(normalizedPathname);
   if (!isB1LessonRoute) return true;
-  return new URLSearchParams(search || "").get("view") === "workbook";
+  return requestedView === "workbook";
 };
 
 const AutoWorkbookStartGuide = () => {
@@ -53,3 +61,5 @@ const AutoWorkbookStartGuide = () => {
 };
 
 export default AutoWorkbookStartGuide;
+
+export const __TESTING__ = { A1_DAY18_CHAPTER122_PATH };
