ad86a7b08fe176efde0ce96f23670fd112b94199
learngermanghana <learngermanghana@gmail.com>
2026-06-25 16:23:07 +0000

    Restore audio player formatting

--- web/src/components/CoursebookAudioPlayer.js
+++ web/src/components/CoursebookAudioPlayer.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-const CoursebookAudioPlayer = ({ url, linkLabel = 'Open audio in a new tab' }) => (
+const CoursebookAudioPlayer = ({ url, linkLabel = "Open audio in a new tab" }) => (
   <a href={url} target="_blank" rel="noreferrer">
     {linkLabel}
   </a>
