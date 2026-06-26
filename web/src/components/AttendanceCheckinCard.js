80794225470789cd9a5d94d20de8345157a25064
learngermanghana <learngermanghana@gmail.com>
2026-06-26 15:54:26 +0000

    Remove unused React import from disabled attendance card

--- web/src/components/AttendanceCheckinCard.js
+++ web/src/components/AttendanceCheckinCard.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 // Student attendance check-in is intentionally disabled inside Falowen.
 // Tutors can continue sharing the external check-in link manually.
 export default function AttendanceCheckinCard() {
