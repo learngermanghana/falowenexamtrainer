4305ed3d4b6bd012c0216f98d8b1ea6080ea4a6c
learngermanghana <learngermanghana@gmail.com>
2026-06-26 15:18:57 +0000

    Add A1 Day 6 group discussion writing box

--- web/src/components/AutoWorkbookStartGuide.js
+++ web/src/components/AutoWorkbookStartGuide.js
@@ -2,11 +2,13 @@ import React, { useMemo, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { styles } from "../styles";
 import { buildWorkbookRouteIndex, normalizeInAppPath } from "../utils/courseWorkbookRoutes";
+import A1Day6FamilyWritingInjector from "./A1Day6FamilyWritingInjector";
 import A1Day18Kapitel122WorkbookPage from "./A1Day18Kapitel122WorkbookPage";
 import CourseWorkbookSubmissionTabs from "./CourseWorkbookSubmissionTabs";
 import WorkbookContextSync from "./WorkbookContextSync";
 import WorkbookStartGuide from "./WorkbookStartGuide";
 
+const A1_DAY6_WORKBOOK_PATH = "/campus/course/a1-day-6-family-and-hobbies-workbook";
 const A1_DAY18_CHAPTER121_PATH = "/campus/course/two-case-prepositions-wechselpraepositionen-day-18";
 const A1_DAY18_CHAPTER122_GRAMMAR_PATH = "/campus/course/a1-12-2-dative-articles-mit-bei-zu";
 const A1_DAY18_CHAPTER122_WORKBOOK_PATH = "/campus/course/a1-day-18-kapitel-12-2-workbook";
@@ -55,34 +57,42 @@ const AutoWorkbookStartGuide = () => {
   const usesSelfManagedSubmissionTabs =
     SELF_MANAGED_WORKBOOK_SUBMISSION_PATHS.has(normalizedPathname)
     || isSelfManagedB1LessonWorkbook(pathname, search);
+  const showA1Day6WritingBox = normalizedPathname === A1_DAY6_WORKBOOK_PATH;
 
   if (normalizedPathname === A1_DAY18_CHAPTER122_WORKBOOK_PATH) return <A1Day18Kapitel122WorkbookPage />;
-  if (!shouldRenderWorkbookGuide({ pathname, search, match })) return null;
+
+  if (!shouldRenderWorkbookGuide({ pathname, search, match })) {
+    return showA1Day6WritingBox ? <A1Day6FamilyWritingInjector /> : null;
+  }
 
   return (
-    <div
-      ref={hostRef}
-      data-auto-workbook-start-guide="true"
-      style={{
-        ...styles.container,
-        display: "grid",
-        width: "100%",
-        minHeight: 0,
-        padding: "0 16px",
-        marginBottom: 12,
-        boxSizing: "border-box",
-      }}
-    >
-      {usesSelfManagedSubmissionTabs ? <WorkbookContextSync match={match} /> : null}
-      <WorkbookStartGuide level={match.level} day={match.day} entry={match.entry} />
-      {usesSelfManagedSubmissionTabs ? null : <CourseWorkbookSubmissionTabs hostRef={hostRef} match={match} />}
-    </div>
+    <>
+      <div
+        ref={hostRef}
+        data-auto-workbook-start-guide="true"
+        style={{
+          ...styles.container,
+          display: "grid",
+          width: "100%",
+          minHeight: 0,
+          padding: "0 16px",
+          marginBottom: 12,
+          boxSizing: "border-box",
+        }}
+      >
+        {usesSelfManagedSubmissionTabs ? <WorkbookContextSync match={match} /> : null}
+        <WorkbookStartGuide level={match.level} day={match.day} entry={match.entry} />
+        {usesSelfManagedSubmissionTabs ? null : <CourseWorkbookSubmissionTabs hostRef={hostRef} match={match} />}
+      </div>
+      {showA1Day6WritingBox ? <A1Day6FamilyWritingInjector /> : null}
+    </>
   );
 };
 
 export default AutoWorkbookStartGuide;
 
 export const __TESTING__ = {
+  A1_DAY6_WORKBOOK_PATH,
   A1_DAY18_CHAPTER121_PATH,
   A1_DAY18_CHAPTER122_GRAMMAR_PATH,
   A1_DAY18_CHAPTER122_WORKBOOK_PATH,
