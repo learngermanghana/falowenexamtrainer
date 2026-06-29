db9224ef27f3cc20175982f3d2b1da1be8183603
learngermanghana <learngermanghana@gmail.com>
2026-06-29 16:05:28 +0000

    Use route-scoped startup services

--- web/src/index.js
+++ web/src/index.js
@@ -4,24 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './i18n';
 import App from './App';
-import SubmitPageLevelGuidanceInjector from './components/SubmitPageLevelGuidanceInjector';
-import SubmitSuccessScreenInjector from './components/SubmitSuccessScreenInjector';
-import MobileHeaderMenuInjector from './components/MobileHeaderMenuInjector';
-import LandingPublicLanguageGuard from './components/LandingPublicLanguageGuard';
 import PublicAuthRouteBridge from './components/PublicAuthRouteBridge';
 import PublicAuthMobileRecovery from './components/PublicAuthMobileRecovery';
-import PublicClassSelectInjector from './components/PublicClassSelectInjector';
-import CourseBookTerminologyInjector from './components/CourseBookTerminologyInjector';
-import LockedSubmissionCardCompactor from './components/LockedSubmissionCardCompactor';
-import B1WorkbookWritingCheatSheetInjector from './components/B1WorkbookWritingCheatSheetInjector';
-import BookPdfDownloadInjector from './components/BookPdfDownloadInjector';
-import AutoGrammarStartGuide from './components/AutoGrammarStartGuide';
 import FalowenRadioSeoPage from './components/FalowenRadioSeoPage';
-import CourseDebugPanel from './components/CourseDebugPanel';
+import RouteScopedAppServices from './components/RouteScopedAppServices';
+import RouteScopedBackgroundServices from './components/RouteScopedBackgroundServices';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext';
 import { ToastProvider } from './context/ToastContext';
-import { registerOfflineServiceWorker } from './serviceWorkerRegistration';
 
 const normalizedPublicPath = window.location.pathname.replace(/\/+$/, '') || '/';
 const isFalowenRadioSeoPage = normalizedPublicPath === '/falowen-radio';
@@ -30,35 +20,21 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <PublicAuthRouteBridge />
+      <PublicAuthMobileRecovery />
+      <RouteScopedBackgroundServices />
       {isFalowenRadioSeoPage ? (
         <FalowenRadioSeoPage />
       ) : (
-        <>
-          <PublicAuthRouteBridge />
-          <PublicAuthMobileRecovery />
-          <AuthProvider>
-            <ToastProvider>
-              <LandingPublicLanguageGuard />
-              <PublicClassSelectInjector />
-              <MobileHeaderMenuInjector />
-              <SubmitPageLevelGuidanceInjector />
-              <SubmitSuccessScreenInjector />
-              <CourseBookTerminologyInjector />
-              <LockedSubmissionCardCompactor />
-              <B1WorkbookWritingCheatSheetInjector />
-              <App />
-              <AutoGrammarStartGuide />
-              <BookPdfDownloadInjector />
-              <CourseDebugPanel />
-            </ToastProvider>
-          </AuthProvider>
-        </>
+        <AuthProvider>
+          <ToastProvider>
+            <RouteScopedAppServices />
+            <App />
+          </ToastProvider>
+        </AuthProvider>
       )}
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
-registerOfflineServiceWorker();
