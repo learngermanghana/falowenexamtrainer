b068ed7c270e61b8d83bb7344a8f2ba31e4a5991
learngermanghana <learngermanghana@gmail.com>
2026-07-01 12:32:40 +0000

    Use the standardized B1 Day 3 workbook directly

--- web/src/components/B1Day3ErfolgsgeschichtenWorkbookPage.js
+++ web/src/components/B1Day3ErfolgsgeschichtenWorkbookPage.js
@@ -1,38 +1,8 @@
 import React from "react";
 import B1Day3ErfolgsgeschichtenWorkbookPageLegacy from "./B1Day3ErfolgsgeschichtenWorkbookPageLegacy";
-import { styles } from "../styles";
-import { WORKBOOK_HOEREN_VIDEOS } from "./WorkbookHoerenVideoConfig";
-import "./workbookHoerenVideoRefresh.css";
-
-const video = WORKBOOK_HOEREN_VIDEOS.b1Day3;
 
 const B1Day3ErfolgsgeschichtenWorkbookPage = () => (
-  <div className="workbook-hoeren-refresh show-hoeren">
-    <div className="legacy-workbook">
-      <B1Day3ErfolgsgeschichtenWorkbookPageLegacy />
-    </div>
-
-    <section className="workbook-hoeren-replacement" style={styles.card}>
-      <h2 style={{ margin: 0 }}>Teil 4 · Hören video</h2>
-      <p style={{ margin: 0, lineHeight: 1.7 }}>
-        Watch and listen to the video, then answer the Teil 4 questions.
-      </p>
-      <a
-        href={video.youtubeUrl}
-        target="_blank"
-        rel="noreferrer"
-        style={{ ...styles.button, width: "fit-content", textDecoration: "none" }}
-      >
-        Open Hören video on YouTube
-      </a>
-      <iframe
-        src={`https://www.youtube.com/embed/${video.youtubeId}?rel=0`}
-        title="B1 Day 3 Erfolgsgeschichten Teil 4 Hören video"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      />
-    </section>
-  </div>
+  <B1Day3ErfolgsgeschichtenWorkbookPageLegacy />
 );
 
 export default B1Day3ErfolgsgeschichtenWorkbookPage;
