c04dd9630cb8737c61ea94c396a154fc0f99254f
learngermanghana <learngermanghana@gmail.com>
2026-07-03 11:11:10 +0000

    Add React startup recovery handling

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './i18n';
 import App from './App';
+import AppStartupBoundary from './components/AppStartupBoundary';
 import PublicAuthRouteBridge from './components/PublicAuthRouteBridge';
 import PublicAuthMobileRecovery from './components/PublicAuthMobileRecovery';
 import FalowenRadioSeoPage from './components/FalowenRadioSeoPage';
@@ -16,26 +17,37 @@ import { ToastProvider } from './context/ToastContext';
 const normalizedPublicPath = window.location.pathname.replace(/\/+$/, '') || '/';
 const isFalowenRadioSeoPage = normalizedPublicPath === '/falowen-radio';
 
+const AppMountedSignal = () => {
+  React.useEffect(() => {
+    window.dispatchEvent(new Event('falowen:app-mounted'));
+  }, []);
+
+  return null;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RouteScopedBackgroundServices />
-      {isFalowenRadioSeoPage ? (
-        <FalowenRadioSeoPage />
-      ) : (
-        <>
-          <PublicAuthRouteBridge />
-          <PublicAuthMobileRecovery />
-          <AuthProvider>
-            <ToastProvider>
-              <RouteScopedAppServices />
-              <App />
-            </ToastProvider>
-          </AuthProvider>
-        </>
-      )}
-    </BrowserRouter>
+    <AppStartupBoundary>
+      <BrowserRouter>
+        <AppMountedSignal />
+        <RouteScopedBackgroundServices />
+        {isFalowenRadioSeoPage ? (
+          <FalowenRadioSeoPage />
+        ) : (
+          <>
+            <PublicAuthRouteBridge />
+            <PublicAuthMobileRecovery />
+            <AuthProvider>
+              <ToastProvider>
+                <RouteScopedAppServices />
+                <App />
+              </ToastProvider>
+            </AuthProvider>
+          </>
+        )}
+      </BrowserRouter>
+    </AppStartupBoundary>
   </React.StrictMode>
 );
 
