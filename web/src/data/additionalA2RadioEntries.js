ad0ea5ef8168391376643fee7ea1b2e934051f24
learngermanghana <learngermanghana@gmail.com>
2026-07-06 20:42:53 +0000

    Move new Small Talk radio video to A2 Day 1

--- web/src/data/additionalA2RadioEntries.js
+++ web/src/data/additionalA2RadioEntries.js
@@ -2,7 +2,7 @@ export const ADDITIONAL_A2_RADIO_ENTRIES = {
   1: {
     key: "a2-day1-small-talk-falowen-radio",
     title: "Small Talk 1.1",
-    youtubeId: "vweIJixJ9QA",
+    youtubeId: "_AHBm9nw_bA",
     duration: "",
     instruction:
       "Höre aufmerksam zu und stimme dich auf Begrüßungen, persönliche Informationen und einfache Small-Talk-Fragen ein. Danach gehst du weiter zu Teil 1.",
@@ -10,7 +10,7 @@ export const ADDITIONAL_A2_RADIO_ENTRIES = {
   2: {
     key: "a2-day2-personen-beschreiben-falowen-radio",
     title: "Personen beschreiben 1.2",
-    youtubeId: "_AHBm9nw",
+    youtubeId: "5UGb1ENJ8Cs",
     duration: "",
     instruction:
       "Höre einfach zu und stimme dich auf das Thema Personen beschreiben ein. Danach gehst du weiter zu Teil 1.",
