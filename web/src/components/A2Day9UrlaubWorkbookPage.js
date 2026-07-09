ba7c1d392c7f295577ad33ebbd98fe1ca3975663
learngermanghana <learngermanghana@gmail.com>
2026-07-09 15:21:32 +0000

    Update A2 day 9 reading and listening tasks

--- web/src/components/A2Day9UrlaubWorkbookPage.js
+++ web/src/components/A2Day9UrlaubWorkbookPage.js
@@ -1,6 +1,30 @@
 import React from "react";
 import A2StandardTabbedWorkbookPage from "./A2StandardTabbedWorkbookPage";
 
+const cultureFreeTimeReadingText = `Kultur und Freizeit in Deutschland
+
+Kultur
+
+Sie mögen Kultur? In den meisten Städten gibt es Museen, Kinos, Theater und Konzertveranstaltungen. Immer mehr Kinos zeigen internationale Filme in der Originalversion (OV). In den Bibliotheken oder Stadtbüchereien können Sie kostenlos oder für wenig Geld Bücher lesen, Musik hören und Filme sehen. Sie können die Bücher, Filme und CDs auch ausleihen: Sie nehmen sie mit nach Hause und bringen sie später wieder zurück.
+
+Volkshochschulen, Vereine und Clubs
+
+An vielen Orten gibt es Volkshochschulen. Dort finden Sie vor allem Kurse für Erwachsene, zum Beispiel Tanzkurse oder Sprachkurse. Sie machen gern Sport? Auch dazu gibt es Kurse an den Volkshochschulen. Aber Sie können auch in ein Schwimmbad oder in einen Sportverein gehen.
+
+Eine andere Möglichkeit sind Vereine und Clubs. In einem Verein sind Menschen mit den gleichen Interessen und Zielen zusammen. Es gibt zum Beispiel Musikvereine, Sportvereine, Kochclubs oder Computerclubs. Es gibt Vereine für Erwachsene und für Jugendliche.
+Für Eltern und Kinder gibt es oft kostenlose Angebote. Für kleine Kinder finden Sie zum Beispiel an manchen Orten Spielgruppen.
+
+Parks und Natur
+
+Sie sind gerne draußen? In jeder Stadt gibt es Parks. Für Kinder gibt es viele Spielplätze. Der Besuch ist meistens kostenlos. In botanischen Gärten können Sie besondere Pflanzen sehen. Tiere aus aller Welt kann man im Zoo besuchen. Botanische Gärten und Zoos kosten normalerweise etwas. Außerdem gibt es in vielen Regionen Seen, Wälder oder Berge, vielleicht wohnen Sie sogar in der Nähe vom Meer.
+
+Im eigenen Zuhause
+
+Zu Hause sehen die meisten Leute gern fern oder hören Radio. Jeder Haushalt in Deutschland muss sein Radio und seinen Fernseher anmelden und dafür eine monatliche Gebühr bezahlen. Im Moment sind das 17,98 Euro im Monat. Wenn man sehr wenig Geld hat, muss man nichts zahlen.
+
+Vielleicht gibt es in Ihrem Haus einen Hof oder einen Balkon. Dort darf man nicht immer alles machen. Zum Beispiel darf man nicht in allen Häusern auf dem Balkon grillen. In der Hausordnung finden Sie alle Informationen dazu.
+Informationen über die Freizeitmöglichkeiten finden Sie auch auf der Internetseite Ihrer Stadt/ Ihres Wohnorts.`;
+
 export default function A2Day9UrlaubWorkbookPage() {
   return (
     <A2StandardTabbedWorkbookPage
@@ -10,17 +34,115 @@ export default function A2Day9UrlaubWorkbookPage() {
       workbookId="A2Day9Urlaub"
       topicPrompt="Sprich über deinen letzten Urlaub oder deinen Traumurlaub."
       schreibenTask="Schreiben Sie eine kurze E-Mail über einen Urlaub. Nennen Sie das Reiseziel, die Unterkunft, Aktivitäten und ein Problem oder eine schöne Erfahrung."
-      lesenText="Letzten Sommer war ich mit meiner Familie am Meer. Wir haben in einem kleinen Hotel gewohnt. Jeden Morgen sind wir schwimmen gegangen. Am Abend haben wir im Restaurant gegessen und Fotos gemacht."
+      lesenText={cultureFreeTimeReadingText}
       lesenQuestions={[
-        { stem: "Wohin ist die Person gereist?", options: ["a) Ans Meer", "b) In die Apotheke", "c) In die Schule", "d) In die Küche"] },
-        { stem: "Wo hat die Familie gewohnt?", options: ["a) In einem kleinen Hotel", "b) Im Zug", "c) Auf der Straße", "d) Im Büro"] },
-        { stem: "Was haben sie am Morgen gemacht?", options: ["a) Sie sind schwimmen gegangen", "b) Sie haben gearbeitet", "c) Sie haben Möbel gekauft", "d) Sie haben gelernt"] },
+        {
+          stem: "Was kann man in Bibliotheken machen?",
+          options: [
+            "a) Nur Bücher kaufen",
+            "b) Musik hören, Bücher lesen, Filme sehen oder ausleihen",
+            "c) Nur CDs anhören",
+            "d) Nur Filme anschauen",
+          ],
+        },
+        {
+          stem: "Wo kann man Sprach- oder Tanzkurse machen?",
+          options: [
+            "a) Im Supermarkt",
+            "b) In der Stadtverwaltung",
+            "c) An der Volkshochschule",
+            "d) Im Museum",
+          ],
+        },
+        {
+          stem: "Was machen Menschen in Vereinen?",
+          options: [
+            "a) Sie wohnen zusammen.",
+            "b) Sie arbeiten dort.",
+            "c) Sie treffen sich, weil sie gemeinsame Interessen haben.",
+            "d) Sie lernen Deutsch.",
+          ],
+        },
+        {
+          stem: "Wo kann man besondere Pflanzen sehen?",
+          options: [
+            "a) Im Kino",
+            "b) Im Zoo",
+            "c) Im botanischen Garten",
+            "d) Im Supermarkt",
+          ],
+        },
+        {
+          stem: "Was ist normalerweise kostenlos?",
+          options: [
+            "a) Der Eintritt in Zoos",
+            "b) Der Besuch von Parks und Spielplätzen",
+            "c) Der Fernseher zu Hause",
+            "d) Die Internetverbindung",
+          ],
+        },
+        {
+          stem: "Wie viel kostet die monatliche Gebühr für Fernsehen und Radio?",
+          options: [
+            "a) 7,98 Euro",
+            "b) 10,50 Euro",
+            "c) 17,98 Euro",
+            "d) Es ist immer kostenlos",
+          ],
+        },
+        {
+          stem: "Wo findet man Informationen zum Grillen auf dem Balkon?",
+          options: [
+            "a) In der Schule",
+            "b) In der Zeitung",
+            "c) In der Hausordnung",
+            "d) Im Fernseher",
+          ],
+        },
       ]}
-      hoerenTask="Höre den Urlaubstext. Notiere Reiseziel, Verkehrsmittel, Unterkunft, Aktivitäten und wichtige Erfahrungen."
+      hoerenTask="Höre den Text über Annas letzten Sommerurlaub. Achte auf das Reiseziel, die Dauer, besondere Orte, Aktivitäten und Annas Wunsch. Submitte deine Antwortbuchstaben im Submit-Tab."
+      hoerenAudioUrl="https://drive.google.com/file/d/1vRaCrQl4QtmYwT8K04JM_A2srofY_d84/view?usp=sharing"
       hoerenQuestions={[
-        { stem: "Welches Thema hat der Hörtext?", options: ["a) Urlaub", "b) Möbel", "c) Medikamente", "d) Grammatik allein"] },
-        { stem: "Welche Aktivität passt zum Urlaub?", options: ["a) Schwimmen gehen", "b) Einen Schrank bauen", "c) Eine Tablette kaufen", "d) Einen Vertrag schreiben"] },
-        { stem: "Was kann man über eine Reise erzählen?", options: ["a) Ziel, Unterkunft und Aktivitäten", "b) Nur den Namen", "c) Nur die Uhrzeit", "d) Nichts"] },
+        {
+          stem: "Wohin ist Anna im letzten Sommerurlaub gereist?",
+          options: [
+            "a) Italien",
+            "b) Griechenland",
+            "c) Spanien",
+          ],
+        },
+        {
+          stem: "Wie lange blieb Anna auf Kreta?",
+          options: [
+            "a) Eine Woche",
+            "b) Zwei Wochen",
+            "c) Drei Tage",
+          ],
+        },
+        {
+          stem: "Was hat Anna besonders gut gefallen?",
+          options: [
+            "a) Die Altstadt von Chania",
+            "b) Der Strand von Elafonissi",
+            "c) Die Berge",
+          ],
+        },
+        {
+          stem: "Was haben Anna und ihre Freunde am letzten Tag gemacht?",
+          options: [
+            "a) Eine Wanderung",
+            "b) Eine Bootstour",
+            "c) Einen Museumsbesuch",
+          ],
+        },
+        {
+          stem: "Was hofft Anna bald wieder zu tun?",
+          options: [
+            "a) Nach Kreta zu reisen",
+            "b) Nach Italien zu reisen",
+            "c) Nach Spanien zu reisen",
+          ],
+        },
       ]}
     />
   );
