bafebc26d53a075b986a6d0b3b25f339e685f37d
learngermanghana <learngermanghana@gmail.com>
2026-07-13 10:37:11 +0000

    Test requested B1 Day 6 and Day 19 radio videos

--- web/src/data/b1RequestedMedia.test.js
+++ web/src/data/b1RequestedMedia.test.js
@@ -3,6 +3,8 @@ import { getB1Day5RadioResource } from "./b1Day5Media";
 import { getLessonRadioResource } from "./lessonRadioDictionary";
 
 it.each([
+  [6, "Leben in der Stadt oder auf dem Land? 2.6", "6zpR_jF26l0"],
+  [19, "Vorstellungsgespräch 6.19", "xgtjgiQBL2k"],
   [22, "Was ist dir in einer Beziehung wichtig? 7.22", "d9vhD_MTrek"],
   [23, "Erstes Date – Typische Situationen 7.23", "76JUgui6CnY"],
 ])("uses the requested B1 Falowen Radio for Day %i", (day, title, youtubeId) => {
