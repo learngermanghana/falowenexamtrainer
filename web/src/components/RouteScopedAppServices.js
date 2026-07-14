df445fe00cd8ddf7c906f526ae9a2a8c2c525a5f
learngermanghana <learngermanghana@gmail.com>
2026-07-14 18:01:55 +0000

    Mount Question of the Day writing templates

--- web/src/components/RouteScopedAppServices.js
+++ web/src/components/RouteScopedAppServices.js
@@ -17,6 +17,7 @@ import UniversalWorkbookLessonNavigator from "./UniversalWorkbookLessonNavigator
 import LockedSubmissionCardCompactor from "./LockedSubmissionCardCompactor";
 import B1WorkbookWritingCheatSheetInjector from "./B1WorkbookWritingCheatSheetInjector";
 import ExamQuestionCheatSheetInjector from "./ExamQuestionCheatSheetInjector";
+import QuestionOfDayWritingTemplateInjector from "./QuestionOfDayWritingTemplateInjector";
 import AutoGrammarStartGuide from "./AutoGrammarStartGuide";
 import BookPdfDownloadInjector from "./BookPdfDownloadInjector";
 import A1CourseExperienceEnhancer from "./A1CourseExperienceEnhancer";
@@ -216,6 +217,7 @@ export default function RouteScopedAppServices() {
       <LockedSubmissionCardCompactor />
       <B1WorkbookWritingCheatSheetInjector />
       <ExamQuestionCheatSheetInjector />
+      <QuestionOfDayWritingTemplateInjector />
       <AutoGrammarStartGuide />
       <BookPdfDownloadInjector />
       {isA1DynamicLesson ? <A1ChapterSpecificLessonStateReset /> : null}
