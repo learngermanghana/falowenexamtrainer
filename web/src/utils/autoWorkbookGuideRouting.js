44e5c62fd0b4405833f3728030648c7501c064a2
learngermanghana <learngermanghana@gmail.com>
2026-07-14 10:44:16 +0000

    Treat A1 chapter 12.3 as self-managed tutor workbook

--- web/src/utils/autoWorkbookGuideRouting.js
+++ web/src/utils/autoWorkbookGuideRouting.js
@@ -4,6 +4,8 @@ export const A1_DAY6_WORKBOOK_PATH =
   "/campus/course/a1-day-6-family-and-hobbies-workbook";
 export const A1_DAY12_WORKBOOK_PATH =
   "/campus/course/a1-day-12-24-hour-clock-and-dates-workbook";
+export const A1_DAY20_CHAPTER123_WORKBOOK_PATH =
+  "/campus/course/letter-writing-intro-german-a1-day-12-3";
 export const A1_DAY18_CHAPTER121_PATH =
   "/campus/course/two-case-prepositions-wechselpraepositionen-day-18";
 export const A1_DAY18_CHAPTER122_GRAMMAR_PATH =
@@ -24,6 +26,7 @@ export const SELF_MANAGED_WORKBOOK_SUBMISSION_PATHS = new Set([
   "/campus/course/a1-day-16-food-and-negation-kapitel-10-workbook",
   A1_DAY6_WORKBOOK_PATH,
   A1_DAY12_WORKBOOK_PATH,
+  A1_DAY20_CHAPTER123_WORKBOOK_PATH,
   A1_DAY18_CHAPTER121_PATH,
   A1_DAY18_CHAPTER122_GRAMMAR_PATH,
   A1_DAY18_CHAPTER122_WORKBOOK_PATH,
@@ -32,8 +35,13 @@ export const SELF_MANAGED_WORKBOOK_SUBMISSION_PATHS = new Set([
   B1_DAY4_WORKBOOK_PATH,
 ]);
 
+const GENERIC_GUIDE_SUPPRESSED_PATHS = new Set([
+  A1_DAY12_WORKBOOK_PATH,
+  A1_DAY20_CHAPTER123_WORKBOOK_PATH,
+]);
+
 export const shouldSuppressGenericWorkbookGuide = (pathname = "") =>
-  normalizeInAppPath(pathname) === A1_DAY12_WORKBOOK_PATH;
+  GENERIC_GUIDE_SUPPRESSED_PATHS.has(normalizeInAppPath(pathname));
 
 export const isSelfManagedB1LessonWorkbook = (pathname = "", search = "") => {
   const normalizedPathname = normalizeInAppPath(pathname);
