68b36a7e6bb4e59be5d69e2aad4ad2f54ddf3e86
learngermanghana <learngermanghana@gmail.com>
2026-07-16 07:04:39 -0700

    Place A1 chapter correction before app route render

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -10,6 +10,7 @@ import PublicAuthMobileRecovery from './components/PublicAuthMobileRecovery';
 import FalowenRadioSeoPage from './components/FalowenRadioSeoPage';
 import RouteScopedAppServices from './components/RouteScopedAppServices';
 import RouteScopedBackgroundServices from './components/RouteScopedBackgroundServices';
+import A1ChapterSpecificLessonRouteBoundary from './components/A1ChapterSpecificLessonRouteBoundary';
 import A1Day13RevisionNumberCleanup from './components/A1Day13RevisionNumberCleanup';
 import A1RadioFirstWorkbookRoutes from './components/A1RadioFirstWorkbookRoutes';
 import RequestedLessonAiVideoHeader from './components/RequestedLessonAiVideoHeader';
@@ -47,21 +48,23 @@ const AuthenticatedAppRoutes = () => (
         <RouteScopedAppServices />
         <A1RadioFirstWorkbookRoutes />
         <RequestedLessonAiVideoHeader />
-        <Routes>
-          <Route
-            path={A1_DAY11_DIRECT_WORKBOOK_PATH}
-            element={<A1Day11DirectWorkbookRoute />}
-          />
-          <Route
-            path={A1_DAY16_CHAPTER9_DIRECT_WORKBOOK_PATH}
-            element={<A1Day16Chapter9DirectWorkbookRoute />}
-          />
-          <Route
-            path={A1_DAY20_CHAPTER123_DIRECT_WORKBOOK_PATH}
-            element={<A1Day20Chapter123DirectWorkbookRoute />}
-          />
-          <Route path="*" element={<App />} />
-        </Routes>
+        <A1ChapterSpecificLessonRouteBoundary>
+          <Routes>
+            <Route
+              path={A1_DAY11_DIRECT_WORKBOOK_PATH}
+              element={<A1Day11DirectWorkbookRoute />}
+            />
+            <Route
+              path={A1_DAY16_CHAPTER9_DIRECT_WORKBOOK_PATH}
+              element={<A1Day16Chapter9DirectWorkbookRoute />}
+            />
+            <Route
+              path={A1_DAY20_CHAPTER123_DIRECT_WORKBOOK_PATH}
+              element={<A1Day20Chapter123DirectWorkbookRoute />}
+            />
+            <Route path="*" element={<App />} />
+          </Routes>
+        </A1ChapterSpecificLessonRouteBoundary>
       </ToastProvider>
     </AuthProvider>
   </>
