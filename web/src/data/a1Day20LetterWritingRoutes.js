d7bce4263205bd9f1f3f301b287c0aa9f789906c
learngermanghana <learngermanghana@gmail.com>
2026-07-16 10:29:01 -0700

    Centralize A1 letter-writing routes

---
+++ web/src/data/a1Day20LetterWritingRoutes.js
@@ -0,0 +1,8 @@
+export const A1_DAY20_CHAPTER123_GRAMMAR_ROUTE =
+  "/campus/course/letter-writing-intro-12-3";
+
+export const A1_DAY20_CHAPTER123_WORKBOOK_ROUTE =
+  "/campus/course/letter-writing-intro-german-a1-day-12-3";
+
+export const A1_DAY20_CHAPTER123_LESSON_ROUTE =
+  "/campus/course/lesson/A1/20?chapter=12.3";
