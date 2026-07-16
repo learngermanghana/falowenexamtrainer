45baecac6a028612d0e1911b2f202bde72322c83
learngermanghana <learngermanghana@gmail.com>
2026-07-16 09:19:14 -0700

    Limit native A1 mode to React-owned shared workbooks

--- web/src/utils/a1NativeWorkbookExperience.js
+++ web/src/utils/a1NativeWorkbookExperience.js
@@ -1,12 +1,17 @@
-import { A1_ASSIGNMENT_REGISTRY } from "../data/a1AssignmentRegistry";
+import { A1_ASSIGNMENT_REGISTRY, getA1AssignmentByRoute } from "../data/a1AssignmentRegistry";
 
 const normalizePath = (value = "") => String(value || "").replace(/\/+$/, "") || "/";
 
-export const A1_NATIVE_STABLE_WORKBOOK_PATHS = new Set([
-  ...Object.values(A1_ASSIGNMENT_REGISTRY).map(({ workbookRoute }) => workbookRoute),
-]);
+export const A1_NATIVE_STABLE_WORKBOOK_PATHS = new Set(
+  Object.values(A1_ASSIGNMENT_REGISTRY)
+    .filter(({ layoutMode }) => layoutMode === "native")
+    .map(({ workbookPath }) => workbookPath),
+);
 
-export const shouldUseNativeA1WorkbookExperience = (pathname = "") =>
-  A1_NATIVE_STABLE_WORKBOOK_PATHS.has(normalizePath(pathname));
+export const shouldUseNativeA1WorkbookExperience = (pathname = "", search = "") =>
+  getA1AssignmentByRoute(normalizePath(pathname), search)?.layoutMode === "native";
+
+export const shouldUseCanonicalA1WorkbookExperience = (pathname = "", search = "") =>
+  Boolean(getA1AssignmentByRoute(normalizePath(pathname), search));
 
 export const __TESTING__ = { normalizePath };
