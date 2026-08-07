930dbb2cf77deb093fa1361d2c3178c2c305c44f
learngermanghana <learngermanghana@gmail.com>
2026-08-07 19:13:49 +0000

    Load standardized B1 speaking structure

--- web/src/index.js
+++ web/src/index.js
@@ -3,4 +3,5 @@ import "./a1WorkbookMobileSubmit.css";
 import "./submissionWordMinimumsRuntime";
 import "./submissionExperienceRuntime";
 import "./studentSubmissionMessagesRuntime";
+import "./b1SpeakingStructureRuntime";
 import "./index.jsx";
