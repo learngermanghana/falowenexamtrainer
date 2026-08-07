9511d071999f3e4e6d820995a34f9728c0d24410
learngermanghana <learngermanghana@gmail.com>
2026-08-07 14:11:54 +0000

    Update approved C1 opinion template to lighter version

--- web/src/data/c1ApprovedOpinionEssayTemplate.js
+++ web/src/data/c1ApprovedOpinionEssayTemplate.js
@@ -1,13 +1,13 @@
 export const C1_APPROVED_OPINION_ESSAY_TEMPLATE = `In der heutigen Zeit wird häufig über [Thema] diskutiert. Dieses Thema ist von großer gesellschaftlicher Bedeutung, da es sowohl [Bereich 1] als auch [Bereich 2] betrifft. Im Folgenden werde ich zunächst erläutern, nach welchen Kriterien [Entscheidung oder Maßnahme] beurteilt werden sollte. Anschließend werde ich anhand eines konkreten Beispiels argumentieren, mögliche Einwände darstellen und schließlich eine Alternative erläutern.
 
-Bei der Beurteilung von [Thema] sollten mehrere Kriterien berücksichtigt werden. Von besonderer Bedeutung ist zunächst [Kriterium 1], da [Begründung]. Darüber hinaus spielt [Kriterium 2] eine entscheidende Rolle, weil [Begründung].
+Bei der Beurteilung von [Thema] sollten mehrere Kriterien berücksichtigt werden. Von besonderer Bedeutung ist zunächst ...
 
-Ein anschauliches Beispiel hierfür ist [Beispiel]. In diesem Fall zeigt sich, dass [Erklärung des Beispiels]. Dadurch wird deutlich, dass [Schlussfolgerung].
+Ein anschauliches Beispiel hierfür ist ... Daran zeigt sich, dass ...
 
-Dennoch gibt es auch Gründe, die gegen [Maßnahme oder Entscheidung] sprechen könnten. Ein wesentlicher Einwand besteht darin, dass [Gegenargument 1]. Außerdem darf nicht außer Acht gelassen werden, dass [Gegenargument 2].
+Andererseits sollte berücksichtigt werden, dass ...
 
-Eine mögliche Alternative bestünde darin, [Alternative]. Im Gegensatz zu [ursprünglicher Maßnahme] hätte diese Lösung den Vorteil, dass [Vorteil der Alternative]. Voraussetzung dafür wäre allerdings, dass [Bedingung].
+Eine mögliche Alternative bestünde darin, ... Ich bin der Auffassung, dass diese Lösung sinnvoller wäre, weil ...
 
-Zusammenfassend lässt sich festhalten, dass [Thema] differenziert betrachtet werden muss. Meines Erachtens ist [eigene Position] sinnvoll, sofern [Bedingung]. Statt ausschließlich auf [eine Lösung] zu setzen, sollte ein ausgewogener Ansatz verfolgt werden, der [Aspekt 1] und [Aspekt 2] miteinander verbindet.`;
+Zusammenfassend lässt sich festhalten, dass [Thema] differenziert betrachtet werden muss. Meines Erachtens sollte eine ausgewogene und sinnvolle Lösung gefunden werden, da sowohl individuelle Bedürfnisse als auch gesellschaftliche Interessen berücksichtigt werden müssen.`;
 
 export default C1_APPROVED_OPINION_ESSAY_TEMPLATE;
