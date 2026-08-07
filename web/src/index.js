cfbab927ea70262d99a4774627809cc69ade98fa
learngermanghana <learngermanghana@gmail.com>
2026-08-07 14:49:14 +0000

    Load student-friendly submission messages

--- web/src/index.js
+++ web/src/index.js
@@ -2,4 +2,5 @@ import "./historyApiSafetyRuntime";
 import "./a1WorkbookMobileSubmit.css";
 import "./submissionWordMinimumsRuntime";
 import "./submissionExperienceRuntime";
+import "./studentSubmissionMessagesRuntime";
 import "./index.jsx";
