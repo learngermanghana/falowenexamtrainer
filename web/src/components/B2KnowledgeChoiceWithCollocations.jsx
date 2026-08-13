7bb9e47a15f7cd734d145928c96567afe3e0e1f0
learngermanghana <learngermanghana@gmail.com>
2026-08-13 13:05:19 +0000

    chore: remove unused B2 collocation wrapper

--- web/src/components/B2KnowledgeChoiceWithCollocations.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import B2KnowledgeChoicePractice from "./B2KnowledgeChoicePractice";
-import B2TopicCollocationPractice from "./B2TopicCollocationPractice";
-
-export default function B2KnowledgeChoiceWithCollocations(props) {
-  return <><B2TopicCollocationPractice day={Number(props?.lesson?.day || 0)} /><B2KnowledgeChoicePractice {...props} /></>;
-}
