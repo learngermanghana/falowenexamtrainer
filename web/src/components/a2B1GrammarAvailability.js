a12df2def5a866fba82d112c67e50ce207229ac6
learngermanghana <learngermanghana@gmail.com>
2026-08-14 18:40:33 +0000

    Register A2 Day 27 grammar notes

--- web/src/components/a2B1GrammarAvailability.js
+++ web/src/components/a2B1GrammarAvailability.js
@@ -1,8 +1,7 @@
 const GRAMMAR_DAYS_BY_LEVEL = Object.freeze({
-  A2: new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 28]),
+  A2: new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 28]),
   B1: new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]),
 });
 
 export const hasA2B1GrammarNotes = (level, day) =>
   Boolean(GRAMMAR_DAYS_BY_LEVEL[String(level || "").toUpperCase()]?.has(Number(day)));
-
