8fd7073c77980baa713f951d4654a30017b69ee5
learngermanghana <learngermanghana@gmail.com>
2026-08-15 18:55:39 +0000

    Load B1 Days 21-25 speaking and writing enhancements

--- web/src/index.js
+++ web/src/index.js
@@ -4,4 +4,5 @@ import "./submissionWordMinimumsRuntime";
 import "./submissionExperienceRuntime";
 import "./studentSubmissionMessagesRuntime";
 import "./b1SpeakingStructureRuntime";
+import "./b1Days21To25SpeakingWritingRuntime";
 import "./index.jsx";
