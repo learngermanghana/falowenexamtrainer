6204f691ee2eaaf3f9a838de1e41b3077d134c2f
learngermanghana <learngermanghana@gmail.com>
2026-09-14 13:53:40 +0000

    Load A1 submission result feedback runtime

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,7 @@
 import "./historyApiSafetyRuntime";
 import "./a1WorkbookMobileSubmit.css";
 import "./a1SubmissionConsistencyRuntime";
+import "./a1SubmissionResultFeedbackRuntime";
 import "./submissionWordMinimumsRuntime";
 import "./submissionExperienceRuntime";
 import "./studentSubmissionMessagesRuntime";
