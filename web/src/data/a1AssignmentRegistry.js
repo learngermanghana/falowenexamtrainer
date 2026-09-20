4e901e6133ee7f220d7b0a0e111d598de8428988
learngermanghana <learngermanghana@gmail.com>
2026-09-20 14:30:53 +0000

    Mark A1-5 workbook as native shared layout

--- web/src/data/a1AssignmentRegistry.js
+++ web/src/data/a1AssignmentRegistry.js
@@ -26,6 +26,7 @@ const NATIVE_SHARED_LAYOUT_COMPONENTS = new Set([
   "A1Day2Kapitel11WorkbookPage",
   "A1Day3PronounsIntroducingYourselfWorkbookPage",
   "A1Chapter3AskingAboutPricesWorkbookPage",
+  "A1Chapter5GermanCasesWorkbookPage",
   "A1Day8CountriesAndLanguagesWorkbookPage",
   "A1Day10ObjectsColorsPossessiveArticlesWorkbookPage",
   "A1Day11UnderstandingTimeWorkbookPage",
