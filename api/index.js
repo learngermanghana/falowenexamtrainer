6bbfe9c159b17a360202991f8f1dcf3ad02158fc
learngermanghana <learngermanghana@gmail.com>
2026-09-26 06:53:28 +0000

    Expose learner support state API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const app = require("../functions/functionz/paymentAwareApp");
 const { publicClassesHandler } = require("../functions/functionz/routes/publicClasses");
 const { classParticipationMeHandler } = require("../functions/functionz/routes/classParticipation");
+const { learnerSupportStateHandler } = require("../functions/functionz/routes/learnerSupport");
 
 module.exports = (req, res) => {
   if (typeof req.url === "string") {
@@ -14,6 +15,10 @@ module.exports = (req, res) => {
 
   if (req.url === "/class-participation/me" || req.url?.startsWith("/class-participation/me?")) {
     return classParticipationMeHandler(req, res);
+  }
+
+  if (req.url === "/support/student-state" || req.url?.startsWith("/support/student-state?")) {
+    return learnerSupportStateHandler(req, res);
   }
 
   return app(req, res);
