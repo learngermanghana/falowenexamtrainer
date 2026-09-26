7a5e32879212d99afef1e38384ab97c175735646
learngermanghana <learngermanghana@gmail.com>
2026-09-26 06:56:38 +0000

    Keep Study Buddy test imports valid

--- web/src/__tests__/StudyBuddyBar.test.js
+++ web/src/__tests__/StudyBuddyBar.test.js
@@ -1,10 +1,11 @@
-const mockNavigate = jest.fn();
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "../i18n";
 import StudyBuddyBar from "../components/StudyBuddyBar";
 import { fetchLearnerSupportState } from "../services/learnerSupportService";
+
+const mockNavigate = jest.fn();
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
